c74d45d6344c6c8648fbadedbd7f348faec15d36
Alon Gvili <34351424+AlonGvili@users.noreply.github.com>
2020-10-26 17:19:24 +0200

    fix

--- src/routes/Pages.tsx
+++ src/routes/Pages.tsx
@@ -1,8 +1,8 @@
 import React, { lazy } from "react";
 import { Route, Switch } from "react-router-dom";
 import Loader from "components/Loader";
-const NotFound = lazy(() => import("pages/NotFound"));
-const UnAuthorized = lazy(() => import("pages/UnAuthorized"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const UnAuthorized = lazy(() => import("../pages/UnAuthorized"));
 const Login = lazy(() => import("pages/LoginPage"));
 
 export default function Pages() {
